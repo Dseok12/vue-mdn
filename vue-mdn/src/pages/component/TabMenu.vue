b71d945d4b77ed9865caab3a2f447a7ee595a977
정원석 <jeong2011528@megastudy.net>
2026-04-02 15:05:37 +0900

    컴포넌트메뉴의 서브메뉴 하드코딩 완료 -> vue컴포넌트로 분리 후 props 반영 예정

--- vue-mdn/src/pages/component/TabMenu.vue
+++ vue-mdn/src/pages/component/TabMenu.vue
@@ -4,15 +4,195 @@
             <p class="page-component__sub-tit">컴포넌트</p>
             <h2 class="page-component__main-tit">탭메뉴</h2>
         </div>
+        <div class="page-component-list__wrap">
+            <div class="page-component-list__box">
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+                <div class="page-component-list__item-wrap">
+                    <div class="page-component-list__item">
+                        <div class="banner-list__item-img">
+                            <a href="/src/assets/img/event_250929_32779.png" target="_blank">
+                                <img src="../../assets/img/event_250929_32779.png" alt="">
+                            </a>
+                        </div>
+                        <div class="page-component-list__item-txt">
+                            <h6 class="page-component-list__item-txt-title">PC 사이트 메인 페이지 배너 가이드 (아이템 1)</h6>
+                            <div class="page-component-list__item-label-box">
+                                <span class="label label-device__type01">PC</span>
+                                <span class="label label-device__type02">MO</span>
+                                <span class="label label-type01__color01">배너</span>
+                                <span class="label label-type01__color01">배너</span>
+                                <span class="label label-type01__color01">배너</span>
+                            </div>
+                            <div class="page-component-list__item-manager">
+                                <strong>담당자 : </strong><span>정원석 대리</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page-component-list__item-code-area">
+                        <a href="javascript:;" @click.prevent="toggleCodeSlide(1)"><span>Css Code Style</span></a>
+                        <div class="codeArea__inner" :class="{ 'codeArea-is-open': activeCodeSlideId === 1 }">
+                            <div class="codeArea__inner--box">
+                                <pre>.banner-style-1 { ... }</pre>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+                <div class="page-component-list__item-wrap">
+                    <div class="page-component-list__item">
+                        <div class="banner-list__item-img">
+                            <a href="/src/assets/img/event_250929_32779.png" target="_blank">
+                                <img src="../../assets/img/event_250929_32779.png" alt="">
+                            </a>
+                        </div>
+                        <div class="page-component-list__item-txt">
+                            <h6 class="page-component-list__item-txt-title">PC 사이트 메인 페이지 배너 가이드 (아이템 2)</h6>
+                            <div class="page-component-list__item-label-box">
+                                <span class="label label-device__type01">PC</span>
+                                <span class="label label-device__type02">MO</span>
+                                <span class="label label-type01__color01">배너</span>
+                                <span class="label label-type01__color01">배너</span>
+                                <span class="label label-type01__color01">배너</span>
+                            </div>
+                            <div class="page-component-list__item-manager">
+                                <strong>담당자 : </strong><span>정원석 대리</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page-component-list__item-code-area">
+                        <a href="javascript:;" @click.prevent="toggleCodeSlide(2)"><span>Css Code Style</span></a>
+                        <div class="codeArea__inner" :class="{ 'codeArea-is-open': activeCodeSlideId === 2 }">
+                            <div class="codeArea__inner--box">
+                                <pre>.banner-style-2 { ... }</pre>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+                <div class="page-component-list__item-wrap">
+                    <div class="page-component-list__item">
+                        <div class="banner-list__item-img">
+                            <a href="/src/assets/img/event_250929_32779.png" target="_blank">
+                                <img src="../../assets/img/event_250929_32779.png" alt="">
+                            </a>
+                        </div>
+                        <div class="page-component-list__item-txt">
+                            <h6 class="page-component-list__item-txt-title">PC 사이트 메인 페이지 배너 가이드 (아이템 3)</h6>
+                            <div class="page-component-list__item-label-box">
+                                <span class="label label-device__type01">PC</span>
+                                <span class="label label-device__type02">MO</span>
+                                <span class="label label-type01__color01">배너</span>
+                                <span class="label label-type01__color01">배너</span>
+                                <span class="label label-type01__color01">배너</span>
+                            </div>
+                            <div class="page-component-list__item-manager">
+                                <strong>담당자 : </strong><span>정원석 대리</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page-component-list__item-code-area">
+                        <a href="javascript:;" @click.prevent="toggleCodeSlide(3)"><span>Css Code Style</span></a>
+                        <div class="codeArea__inner" :class="{ 'codeArea-is-open': activeCodeSlideId === 3 }">
+                            <div class="codeArea__inner--box">
+                                <pre>.banner-style-3 { ... }</pre>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+                <div class="page-component-list__item-wrap">
+                    <div class="page-component-list__item">
+                        <div class="banner-list__item-img">
+                            <a href="/src/assets/img/event_250929_32779.png" target="_blank">
+                                <img src="../../assets/img/event_250929_32779.png" alt="">
+                            </a>
+                        </div>
+                        <div class="page-component-list__item-txt">
+                            <h6 class="page-component-list__item-txt-title">PC 사이트 메인 페이지 배너 가이드 (아이템 4)</h6>
+                            <div class="page-component-list__item-label-box">
+                                <span class="label label-device__type01">PC</span>
+                                <span class="label label-device__type02">MO</span>
+                                <span class="label label-type01__color01">배너</span>
+                                <span class="label label-type01__color01">배너</span>
+                                <span class="label label-type01__color01">배너</span>
+                            </div>
+                            <div class="page-component-list__item-manager">
+                                <strong>담당자 : </strong><span>정원석 대리</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page-component-list__item-code-area">
+                        <a href="javascript:;" @click.prevent="toggleCodeSlide(4)"><span>Css Code Style</span></a>
+                        <div class="codeArea__inner" :class="{ 'codeArea-is-open': activeCodeSlideId === 4 }">
+                            <div class="codeArea__inner--box">
+                                <pre>.banner-style-4 { ... }</pre>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+                <div class="page-component-list__item-wrap">
+                    <div class="page-component-list__item">
+                        <div class="banner-list__item-img">
+                            <a href="/src/assets/img/event_250929_32779.png" target="_blank">
+                                <img src="../../assets/img/event_250929_32779.png" alt="">
+                            </a>
+                        </div>
+                        <div class="page-component-list__item-txt">
+                            <h6 class="page-component-list__item-txt-title">PC 사이트 메인 페이지 배너 가이드 (아이템 5)</h6>
+                            <div class="page-component-list__item-label-box">
+                                <span class="label label-device__type01">PC</span>
+                                <span class="label label-device__type02">MO</span>
+                                <span class="label label-type01__color01">배너</span>
+                                <span class="label label-type01__color01">배너</span>
+                                <span class="label label-type01__color01">배너</span>
+                            </div>
+                            <div class="page-component-list__item-manager">
+                                <strong>담당자 : </strong><span>정원석 대리</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page-component-list__item-code-area">
+                        <a href="javascript:;" @click.prevent="toggleCodeSlide(5)"><span>Css Code Style</span></a>
+                        <div class="codeArea__inner" :class="{ 'codeArea-is-open': activeCodeSlideId === 5 }">
+                            <div class="codeArea__inner--box">
+                                <pre>.banner-style-5 { ... }</pre>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+                <!-- 추후 컴포넌트로 만들어서 props 연결 학습 예정 -->
+            </div>
+        </div>
     </div>
 </template>
+
 <script>
 import '../../css/pages/component/common.css';
+import '../../css/pages/component/banner.css';
 
 export default {
-    
+    data() {
+        return {
+            // 초기값 null은 아무것도 열리지 않은 상태입니다.
+            activeCodeSlideId: null,   
+        };
+    },
+    methods : {
+        toggleCodeSlide(id) {
+            // 1. 이미 열려있는 항목을 다시 누르면 닫습니다. (null 처리)
+            // 2. 다른 항목을 누르면 해당 항목의 ID로 교체합니다. (그 항목만 열림)
+            if (this.activeCodeSlideId === id) {
+                this.activeCodeSlideId = null;
+            } else {
+                this.activeCodeSlideId = id;
+            }
+        }
+    }
 }
 </script>
-<style>
-    
-</style>
