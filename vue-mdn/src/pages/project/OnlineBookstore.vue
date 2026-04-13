628daab8a551979fcd3e0e865f00cd09f0c43239
정원석 <jeong2011528@megastudy.net>
2026-04-13 11:03:39 +0900

    프로젝트 컴포넌트 작업(메가선생님)

--- vue-mdn/src/pages/project/OnlineBookstore.vue
+++ vue-mdn/src/pages/project/OnlineBookstore.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
-        <div class="page-project__tit-box">
-            <div class="page-project__sub-tit">프로젝트</div>
-            <h1 class="page-project__tit">온라인서점</h1>
+        <div class="page-project__top-wrap">
+            <div class="page-project__title-wrap">
+                <p class="page-project__sub-tit">프로젝트</p>
+                <h2 class="page-project__main-tit">온라인서점</h2>
+            </div>
+            <div class="page-project-btn-wrap">
+                <router-link to="../posting/post" class="page-project__add-post__btn" >게시글 등록</router-link>
+            </div>
         </div>
     </div>
 </template>
